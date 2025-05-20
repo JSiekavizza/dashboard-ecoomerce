bb727f1e5fc69dcf5c1ce9fb2ab85e65304f29d7
Jhonatan Siekavizza <jsieka2789@gmail.com>
2025-05-20 18:02:30 -0500

    Agregar variables de entorno Railwy & Vercel

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,9 @@ const app = express();
 // Middlewares
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin:
+      process.env.FRONTEND_URL ||
+      "https://dashboard-ecoomerce-dev-sieka.vercel.app/",
   })
 );
 app.use(express.json());
